d25beed28a7e4748ca276cea6990cd83cca0faca
headit74 <headit74@outlook.com>
2020-03-03 04:17:25 +0800

    8. Tap Event Handling...

--- lib/location_list.dart
+++ lib/location_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lesson01/location_detail.dart';
 import 'models/location.dart';
 import 'styles.dart';
 
@@ -18,15 +19,27 @@ class LocationList extends StatelessWidget {
       ),
       body: ListView.builder(
         itemCount: this._locations.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            contentPadding: EdgeInsets.all(10.0),
-            leading: _itemThumbnail(this._locations[index]),
-            title: _itemTitle(this._locations[index]),
-          );
-        },
+        itemBuilder: _listViewItemBuilder,
       ),
     );
+  }
+
+  Widget _listViewItemBuilder(BuildContext context, int index) {
+    var location = this._locations[index];
+    return ListTile(
+      contentPadding: EdgeInsets.all(10.0),
+      leading: _itemThumbnail(this._locations[index]),
+      title: _itemTitle(this._locations[index]),
+      onTap: () => _navigationToLocationDetail(context, this._locations[index]),
+    );
+  }
+
+  void _navigationToLocationDetail(BuildContext context, Location location) {
+    // print('>>>>>>>> ${this._locations[index].name}');
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => LocationDetail(location)));
   }
 
   Widget _itemThumbnail(Location location) {
@@ -37,6 +50,9 @@ class LocationList extends StatelessWidget {
   }
 
   Widget _itemTitle(Location location) {
-    return Text(' ${location.name}', style: Styles.textDefault,);
+    return Text(
+      ' ${location.name}',
+      style: Styles.textDefault,
+    );
   }
 }
